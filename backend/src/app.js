c67c2ccb8ef5d3d714bcccdd1a00843c2a5dd0f9
cecabrerac <ccabrera9@hotmail.com>
2025-12-20 17:10:12 -0500

    last changes. backend working

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", async (req, res) => {
+  res.send("Hello World!");
+});
+
 app.get("/products", async (req, res) => {
   const result = await pool.query("SELECT * FROM products");
   res.json(result.rows);
