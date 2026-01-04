962a50fa8f7790bfe22b4e28694820a33b4e813d
cecabrerac <ccabrera9@hotmail.com>
2026-01-04 15:52:20 -0500

    primereact working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./App.jsx";
 
 // PrimeReact CSS
+// import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
